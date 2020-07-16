43e02650b1f3c932aa0ad72554fc34820c83a27e
MrTops <43430084+MrTops@users.noreply.github.com>
2020-07-16 18:10:36 -0400

    compartmentalized main.py

--- main.py
+++ main.py
@@ -1,12 +1,59 @@
 import Input
+import time
+import os
+from AssetManager import AssetManager
+
+keys = None
+
+def play():
+    AssetManager.clearScreen()
+    print(AssetManager.grabAsset('playMenu'))
+    @keys.hookKeyEventDecorator('r')
+    def event(down):
+        keys.unhookAll()
+        mainMenu()
+
+def settings():
+    AssetManager.clearScreen()
+    print(AssetManager.grabAsset('settingsMenu'))
+    @keys.hookKeyEventDecorator('r')
+    def event(down):
+        keys.unhookAll()
+        mainMenu()
+
+def mainMenu():
+    AssetManager.clearScreen()
+    print(AssetManager.grabAsset('mainMenu'))
+
+    @keys.hookKeyEventDecorator('p')
+    def event(down):
+        keys.unhookAll()
+        play()
+
+    @keys.hookKeyEventDecorator('e')
+    def event(down):
+        keys.unhookAll()
+        AssetManager.clearScreen()
+        print('Thank you for playing')
+        for num in range(0, 3):
+            print('.')
+            time.sleep(1)
+        print('closing')
+        os.abort()
+    
+    @keys.hookKeyEventDecorator('s')
+    def event(down):
+        keys.unhookAll()
+        settings()
+        
+        
 
 def main():
+    AssetManager.clearScreen()
+    global keys
     keys = Input.Input()
-    Input.Input.genAliveThread()
-    @keys.hookKeyEventDecorator('e')
-    def func(pressed):
-        print('E {}'.format(pressed))
-    
     keys.start()
+    Input.Input.genAliveThread()
+    mainMenu()
 
 main()
