e73384f49c839dfd1cacbf6117f7568e715e6272
MrTops <43430084+MrTops@users.noreply.github.com>
2020-07-16 23:38:59 -0400

    added generateBoxedText and rawGrabAsset

--- AssetManager.py
+++ AssetManager.py
@@ -5,18 +5,42 @@ class AssetManager(object):
     This class is ment to not be instanced. Please only use the static methods
     """
     @staticmethod
-    def grabAsset(filename):
+    def rawGrabAsset(filelocation):
         """
-            grab asset from assets that is a .txt
+            grbas a asset from a file location rather than the ./assets/{}.txt
         """
-        f = open('./assets/{}.txt'.format(filename))
+        f = open(filelocation)
         lines = f.readlines()
         string = ""
         f.close()
         for line in lines:
             string += line
         return string
+        
+
+    @staticmethod
+    def grabAsset(filename):
+        """
+            grab asset from assets that is a .txt
+        """
+        return AssetManager.rawGrabAsset('./assets/{}.txt'.format(filename))
     
+    @staticmethod
+    def generateBoxedText(message):
+        """
+            Generates a boxed message
+        """
+        splitMessage = message.split('\n')
+        longestSegment = None
+        for section in splitMessage:
+            if len(section) >= len(longestSegment or section):
+                longestSegment = section
+        string = "+={}=+\n".format(len(longestSegment)*"=")
+        for section in splitMessage:
+            string += "| {}{} |\n".format(section, (len(longestSegment)-len(section))*" ")
+        string += "+={}=+\n".format(len(longestSegment)*"=")
+        return string
+
     @staticmethod
     def clearScreen():
         """
