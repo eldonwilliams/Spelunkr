fb54299baa34bf1e98baee82068ef0e4c9ad6378
MrTops <43430084+MrTops@users.noreply.github.com>
2020-07-16 23:38:21 -0400

    sent to mods

---
+++ Game.py
@@ -0,0 +1,25 @@
+import AssetManager
+import Input
+import Material
+import Registry
+import PlayerData
+import Area
+import Globals
+import EventHandler
+from Registry import Registry
+
+class Game(Registry):
+    """
+        Contains most modules of Spelunkr
+        *extends of Registry*
+    """
+    def __init__(self):
+        self.reg = {}
+        self.registerItem('AssetManager', AssetManager)
+        self.registerItem('Input', Input)
+        self.registerItem('Material', Material)
+        self.registerItem('Registry', Registry)
+        self.registerItem('PlayerData', PlayerData)
+        self.registerItem('Area', Area)
+        self.registerItem('Globals', Globals)
+        self.registerItem('EventHandler', EventHandler)
