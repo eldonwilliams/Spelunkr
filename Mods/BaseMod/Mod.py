69088651fa1255d1ecbde07f5ea7559bcd1db634
MrTops <43430084+MrTops@users.noreply.github.com>
2020-07-16 23:37:25 -0400

    Create Mod.py

---
+++ Mods/BaseMod/Mod.py
@@ -0,0 +1,13 @@
+"""
+    Index for the base mod
+"""
+
+info = {
+    "modname": "Spelunkr",
+    "modid": "spelunkr",
+    "description": "Spelunkr's base game assets.",
+    "author": "Spelunkr Team"
+}
+
+def Load(game):
+    pass
