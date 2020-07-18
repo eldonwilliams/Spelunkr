528febbfa187a32ccab6ee3f2b5368ca30be3c31
MrTops <43430084+MrTops@users.noreply.github.com>
2020-07-18 16:30:39 -0400

    Update PlayerData.py

--- PlayerData.py
+++ PlayerData.py
@@ -9,7 +9,7 @@ class PlayerData(object):
         """
             Generates a PlayerData object from data given at the FileLocation
         """
-        return PlayerData(json.load(open(fileLocation, 'r')))
+        return PlayerData(json.load(open(fileLocation, 'r')), filename=fileLocation)
 
     @staticmethod
     def saveToJson(playerData, fileLocation):
@@ -20,9 +20,10 @@ class PlayerData(object):
         f.write(json.dumps(playerData.__dict__))
         f.close()
 
-    def __init__(self, jsonData):
+    def __init__(self, jsonData, filename=""):
         def get(key, other): return (jsonData[key] if key in jsonData else other)
-        self.currentArea = get('CurrentArea', 'NONE')
         self.credits = get('Credits', 0)
+        self.unlockedAreas = get('UnlockedAreas', [])
+        self.filename = filename
         self.strength = get('Strength', 1)
         
