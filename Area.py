9531b87233ef6ba8fb51c1b22d8a184fba6d860c
MrTops <43430084+MrTops@users.noreply.github.com>
2020-07-18 16:28:06 -0400

    add cost and getRandomMaterial

--- Area.py
+++ Area.py
@@ -1,12 +1,19 @@
+import random
 from Registry import Registry
 
 class Area(object):
     """
         An Area is a place the player can be and contains materials the player can mine
     """
-    def __init__(self, name, modid):
+    def __init__(self, name, cost, modid):
         self.name = name
         self.modid = modid
-        self.Materals = Registry()
+        self.cost = cost
+        self.Materials = Registry()
 
-
+    def getRandomMaterial(self):
+        """
+            Returns a random material from self.Materials
+        """
+        materialArray = list(self.Materials.reg.values())
+        return materialArray[random.randint(0, len(materialArray)-1)]
