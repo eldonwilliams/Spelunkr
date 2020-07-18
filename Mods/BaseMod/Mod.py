88f7ce17b0197343e21728b1bd73ecc8b9266c23
MrTops <43430084+MrTops@users.noreply.github.com>
2020-07-18 16:30:29 -0400

    added some things to base mod

--- Mods/BaseMod/Mod.py
+++ Mods/BaseMod/Mod.py
@@ -10,4 +10,20 @@ info = {
 }
 
 def Load(game):
-    pass
+    Area = game.getItem("Area")
+    Material = game.getItem("Material")
+    AssetManager = game.getItem("AssetManager")
+    Registry = game.getItem("Registry")
+
+    MaterialRegistry = Registry.MaterialRegistry
+    AreaRegistry = Registry.AreaRegistry
+
+    forestArea = Area.Area(name='The Forest', modid=info["modid"], cost=0)
+    testArea1 = Area.Area(name='TA1', modid=info["modid"], cost=10)
+    testArea2 = Area.Area(name='TA2', modid=info["modid"], cost=75)
+    pebbleMaterial = Material.Material(name='Pebble', health=10, area=forestArea, value=1, asset='pebble', modid=info["modid"])
+
+    MaterialRegistry.registerItem(pebbleMaterial.name, pebbleMaterial)
+    AreaRegistry.registerItem(forestArea.name, forestArea)
+    AreaRegistry.registerItem(testArea1.name, testArea1)
+    AreaRegistry.registerItem(testArea2.name, testArea2)
